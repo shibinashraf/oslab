e6e995ecbaf38df8e2042d21b16fea6cab464ce1
shibin <shibinashraf36@gmail.com>
2021-09-28 19:04:32 +0530

    Add files via upload

---
+++ procompipes.c
@@ -0,0 +1,23 @@
+//To implement the interprocess communication using pipes.
+#include<stdio.h>
+int main()
+{
+int fd[2],child;
+char a[10];
+printf("\n enter the string to enter into the pipe:");
+scanf("%s",a);
+pipe(fd);
+child=fork();
+if(!child)
+{ close(fd[10]);
+write(fd[1],a,5);
+wait(0);
+}
+else
+{
+close(fd[1]);
+read(fd[0],a,5);
+printf("\n the string retrived from pipe is %s\n",a);
+}
+return 0;
+}
